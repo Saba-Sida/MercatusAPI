923e96a96b73fb54474e0150161d8b0f65d96c55
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-03-29 21:29:56 +0400

    Adding Api response wrapper, and external method to format into api ready type

---
+++ MercatusAPI/ExternalMethods/ControllerExternalMethods.cs
@@ -0,0 +1,29 @@
+﻿using MercatusAPI.Models.ResponseModels.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercatusAPI.ExternalMethods;
+
+public static class ControllerExternalMethods
+{
+    /// <summary>
+    /// Returns api ready response type from ApiResponse Wrapper
+    /// </summary>
+    /// <param name="controllerBase"></param>
+    /// <param name="apiResponse"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IActionResult FormatResponse<T>(this ControllerBase controllerBase, ApiResponse<T> apiResponse)
+    {
+        if (apiResponse.IsSuccess)
+        {
+            if (apiResponse.Data != null)
+            {
+                return controllerBase.StatusCode((int)apiResponse.StatusCode, apiResponse.Data);
+            }
+
+            return controllerBase.StatusCode((int)apiResponse.StatusCode);
+        }
+
+        return controllerBase.StatusCode((int)apiResponse.StatusCode, apiResponse.ErrorMessage);
+    }
+}
---
+++ MercatusAPI/Models/ResponseModels/Shared/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace MercatusAPI.Models.ResponseModels.Shared;
+
+public record ApiResponse<T>(
+    T? Data,
+    bool IsSuccess,
+    HttpStatusCode StatusCode,
+    string ErrorMessage
+    );
