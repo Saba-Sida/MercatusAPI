2547cf02df83ec39c11376f6e707e0e77b4ca058
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-03-29 22:19:10 +0400

    Added OperationResponse wrapper model in application layer

---
+++ Application/Models/Common/OperationResponseStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Models.Common;
+
+public enum OperationResponseStatus
+{
+    Success = 200,
+    NotFound = 404,
+    ValidationError = 400,
+    Conflict = 409,
+    Unauthorized = 401,
+    Forbidden = 403,
+    ServerError = 500
+}
