80e38ae64fef63cac959cc56fbcb35a05f40b951
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-03-29 19:24:03 +0400

    Creating a simple interface and implemented class of cache memory functionality

---
+++ Infrastructure/Services/InMemoryCacheService.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Infrastructure.Services;
+
+public class InMemoryCacheService: ICacheService
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public InMemoryCacheService(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+    
+    public Task SetValueByKey<T>(string key, T value, TimeSpan timeToLive)
+    {
+        _memoryCache.Set(key, value, timeToLive);
+        return Task.CompletedTask;
+    }
+
+    public Task<T?> GetValueByKey<T>(string key)
+    {
+        return Task.FromResult(_memoryCache.Get<T>(key));
+    }
+}
