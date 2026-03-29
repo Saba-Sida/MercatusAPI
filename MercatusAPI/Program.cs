db97a53cf6e279a5824a272d2de6c7fb90da1e80
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-03-29 18:12:40 +0400

    Registering Email Sender in DI

--- MercatusAPI/Program.cs
+++ MercatusAPI/Program.cs
@@ -1,6 +1,8 @@
 using Application.Security;
+using Application.Services;
 using Infrastructure.Data;
 using Infrastructure.Security;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IPasswordHasher, Sha256PasswordHasher>();
+builder.Services.AddScoped<IEmailSender, SmtpEmailSender>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
