2edcf55f06deaac474551d210cb2f0bc419cb9e3
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-03-29 15:42:00 +0400

    Creating basic AppDbContext and registering in program.cs, registering User entity there, defining basic User entity configuration

---
+++ Infrastructure/EntityConfigs/UserEntityConfig.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityConfigs;
+
+public class UserEntityConfig: IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(user => user.UserId);
+    }
+}
