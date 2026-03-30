b56e7839e2f701a2cbb4df6d90aa59ed05b87b67
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-03-31 01:57:35 +0400

    Adding Users Repository

---
+++ Application/Repositories/Users/IUsersRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Repositories.Users;
+
+public interface IUsersRepository
+{
+    Task<bool> EmailExistsInDb(string email);
+    Task AddNewUser(User user);
+}
---
+++ Infrastructure/Repositories/Users/UsersRepository.cs
@@ -0,0 +1,27 @@
+﻿using Application.Repositories.Users;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Users;
+
+public class UsersRepository: IUsersRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public UsersRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<bool> EmailExistsInDb(string email)
+    {
+        return await _dbContext.Users.AnyAsync(user => user.Email == email);
+    }
+
+    public async Task AddNewUser(User user)
+    {
+        await _dbContext.Users.AddAsync(user);
+        await _dbContext.SaveChangesAsync();
+    }
+}
