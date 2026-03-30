f8e670f7215f9e35d516a368acabce76e0f52317
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-03-31 01:55:43 +0400

    Adding registration specific models

---
+++ Application/Models/RequestModels/VerifyEmailRequest.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.RequestModels;
+
+public record VerifyEmailRequest(
+    [Required] string Email,
+    [Required] string Otp
+    );
