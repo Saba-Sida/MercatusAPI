afa1cdffc3549d13b72c57716d1c807c6de43a20
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-03-31 01:59:02 +0400

    Adding basic User Auth Validator for current needs

---
+++ Application/Validation/IUserAuthValidator.cs
@@ -0,0 +1,8 @@
+﻿using MercatusAPI.Models.RequestModels;
+
+namespace Application.Validation;
+
+public interface IUserAuthValidator
+{
+    Task<(bool IsValid, string ErrorMessage)> ValidateSignUpRequest(SignUpRequest request);
+}
