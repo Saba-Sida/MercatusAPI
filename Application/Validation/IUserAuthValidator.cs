f7261dda886e60fca4cbce5749b37e4cc16d49a0
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-04-01 03:27:22 +0400

    Adding Log In request validation

--- Application/Validation/IUserAuthValidator.cs
+++ Application/Validation/IUserAuthValidator.cs
@@ -1,8 +1,11 @@
-﻿using MercatusAPI.Models.RequestModels;
+﻿using Application.Models.DTOs;
+using Application.Models.RequestModels;
+using MercatusAPI.Models.RequestModels;
 
 namespace Application.Validation;
 
 public interface IUserAuthValidator
 {
     Task<(bool IsValid, string ErrorMessage)> ValidateSignUpRequest(SignUpRequest request);
+    Task<(bool IsValid, string ErrorMessage)> VerifyLogInRequest(LogInRequest request);
 }
