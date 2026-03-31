3ecd1a9c17fdd50523f1ceae6314dfdf57b2493f
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-04-01 03:31:22 +0400

    Added fully functional log in endpoint in Users Controller

--- MercatusAPI/Controllers/UsersController.cs
+++ MercatusAPI/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Application.Models.RequestModels;
 using Application.Services.Users;
 using MercatusAPI.ExternalMethods;
 using MercatusAPI.Models.RequestModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MercatusAPI.Controllers;
@@ -32,6 +33,16 @@ public class UsersController: ControllerBase
     public async Task<IActionResult> VerifyEmail([FromBody] VerifyEmailRequest request)
     {
         OperationResponse response = await _userService.VerifyEmailByOtp(request);
+
+        return this.FormatResponse(response);
+    }
+
+    /// <returns>If successful: JWT token for user</returns>
+    [HttpPost]
+    [Route("log-in")]
+    public async Task<IActionResult> LogIn([FromBody] LogInRequest request)
+    {
+        OperationResponse<string> response = await _userService.LogIn(request);
 
         return this.FormatResponse(response);
     }
