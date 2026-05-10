bdc76dfdfa13149bf0deefe4b7bc5e1e267c69ed
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-11 00:50:35 +0400

    Added new request model for category creating operation

---
+++ Application/Models/RequestModels/AddNewCategoryRequest.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.RequestModels;
+
+public record AddNewCategoryRequest(
+    [Required] string NewCategoryName,
+    int? CategoryParentId = null
+    );
