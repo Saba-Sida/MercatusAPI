ddbd1cda6df7931a138e8b317547fc18a5bcbea5
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-11 00:54:04 +0400

    Added repository for category operations with database

---
+++ Application/Repositories/Categories/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Repositories.Categories;
+
+public interface ICategoryRepository
+{
+    Task<bool> CategoryNameExists(string name);
+    Task<bool> CategoryByIdExists(int categoryId);
+    Task<int> AddNewCategory(string categoryName, int? categoryParentId = null);
+}
---
+++ Infrastructure/Repositories/Categories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Application.Repositories.Categories;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Categories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public CategoryRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<bool> CategoryNameExists(string name)
+    {
+        return await _dbContext.Categories
+            .AnyAsync(category => category.CategoryName == name);
+    }
+
+    public async Task<bool> CategoryByIdExists(int categoryId)
+    {
+        return await _dbContext.Categories
+            .AnyAsync(category => category.CategoryId == categoryId);
+    }
+
+    public async Task<int> AddNewCategory(string categoryName, int? categoryParentId = null)
+    {
+        var newCategory = new Category
+        {
+            CategoryName = categoryName,
+            CategoryParentId = categoryParentId
+        };
+
+        _dbContext.Categories.Add(newCategory);
+        await _dbContext.SaveChangesAsync();
+
+        return newCategory.CategoryId;
+    }
+}
