a3b0d461f2fffd5173d31e8886d6914328c3ae65
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-11 00:56:54 +0400

    Added category controller with [post] endpoint .../add

---
+++ MercatusAPI/Controllers/Admin/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Models.RequestModels;
+using Application.Services.Categories;
+using MercatusAPI.ExternalMethods;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercatusAPI.Controllers.Admin;
+
+
+[Authorize(Policy = "AdminPolicy")]
+[ApiController]
+[Route("api/admin/[controller]")]
+public class CategoryController: ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+    
+    [HttpPost]
+    [Route("add")]
+    public async Task<IActionResult> AddNewCategory([FromBody] AddNewCategoryRequest request)
+    {
+        var newCategoryCreationResponse = await _categoryService.CreateNewCategory(request);
+
+        return this.FormatResponse(newCategoryCreationResponse);
+    }
+}
