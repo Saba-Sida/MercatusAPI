01d5583b95501c6c5cb02dc81ba144d87e9f591d
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 22:18:54 +0400

    Added Helper class for converting photos

---
+++ MercatusAPI/LayerSpecificHelpers/PhotoConvertingHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace MercatusAPI.LayerSpecificHelpers;
+
+public class PhotoConvertingHelper
+{
+    public async Task<(byte[], string)?> ConvertIFormFileToByteArrayAndFileExtension(IFormFile? file)
+    {
+        if (file == null) return null;
+        
+        using var ms = new MemoryStream();
+        await file.CopyToAsync(ms);
+        
+        var fileByteArray = ms.ToArray();
+        var fileExtension = Path.GetExtension(file.FileName);
+        
+        return (fileByteArray, fileExtension);
+    }
+}
