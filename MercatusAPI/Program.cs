ee5980de5a86139ce16eb8a0f97ccc8d08fd66fb
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 18:45:04 +0400

    Added cors policy for the current configuration

--- MercatusAPI/Program.cs
+++ MercatusAPI/Program.cs
@@ -71,6 +71,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -81,6 +92,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngularApp");
 
 app.UseAuthentication();
 app.UseAuthorization();
