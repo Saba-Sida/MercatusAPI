34fbf36e1abe224940ac23850c21e9f0cd9cec70
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 22:14:08 +0400

    Added generic storage manager helper class

---
+++ Application/BlobStorage/IGenericBlobStorageManager.cs
@@ -0,0 +1,25 @@
+﻿namespace Application.BlobStorage;
+
+/// <summary>
+/// Generic Blob storage manager
+/// </summary>
+/// <remarks>
+/// This file storage manager performs the storage operations safely,
+/// meaning in case of any error and unsuccessful storing or file retrieval operation,
+/// program won't crash, rather it will calmly return default value in every method where logical
+/// </remarks>
+public interface IGenericBlobStorageManager
+{
+    /// <summary>
+    /// Saves file to given address
+    /// </summary>
+    /// <param name="addressToSave">
+    /// Directory full address, including the directory name in the end
+    /// </param>
+    /// <param name="fullFileName">Complete file name</param>
+    /// <remarks>
+    /// If such address does not exist, creates.
+    /// If such file already exists, overwrites
+    /// </remarks>
+    Task<string?> SaveFile(string addressToSave, string fullFileName, byte[]? content);
+}
---
+++ Infrastructure/BlobStirage/GenericBlobStorageManager.cs
@@ -0,0 +1,35 @@
+﻿using Application.BlobStorage;
+
+namespace Infrastructure.BlobStirage;
+
+public class GenericBlobStorageManager : IGenericBlobStorageManager
+{
+    public async Task<string?> SaveFile(string addressToSave, string fullFileName, byte[]? content)
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(addressToSave) ||
+                string.IsNullOrWhiteSpace(fullFileName) ||
+                content == null ||
+                content.Length == 0)
+            {
+                return null;
+            }
+
+            if (!Directory.Exists(addressToSave))
+            {
+                Directory.CreateDirectory(addressToSave);
+            }
+
+            var fullPath = Path.Combine(addressToSave, fullFileName);
+
+            await File.WriteAllBytesAsync(fullPath, content);
+
+            return fullPath;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
