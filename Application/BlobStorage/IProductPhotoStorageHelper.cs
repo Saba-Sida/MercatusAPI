185515478138c030392fd11222e528f1a3d0c3d8
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 23:40:09 +0400

    Created Product photo manager interface and class

---
+++ Application/BlobStorage/IProductPhotoStorageHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.BlobStorage;
+
+public interface IProductPhotoStorageHelper
+{
+    Task<List<string>> SaveProductPhotos(int productId, List<(byte[], string)> photos);
+}
---
+++ Infrastructure/BlobStirage/ProductPhotoStorageHelper.cs
@@ -0,0 +1,41 @@
+﻿using Application.BlobStorage;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.BlobStirage;
+
+public class ProductPhotoStorageHelper : IProductPhotoStorageHelper
+{
+    private readonly IGenericBlobStorageManager _genericBlobStorageManager;
+    private readonly string _baseProductPhotosStorageAddress;
+    
+    public ProductPhotoStorageHelper(
+        IGenericBlobStorageManager genericBlobStorageManager,
+        IConfiguration configuration
+    )
+    {
+        _genericBlobStorageManager = genericBlobStorageManager;
+        var baseUri = configuration["BlobStorage:BaseUri"]!;
+        var productPhotosMainSubDirectory = configuration["BlobStorage:ProductPhotosMainSubDirectory"]!;
+
+        _baseProductPhotosStorageAddress =
+            Path.Combine(baseUri, productPhotosMainSubDirectory); // not ends with slash
+    }
+    
+    public async Task<List<string>> SaveProductPhotos(int productId, List<(byte[], string)> photos)
+    {
+        var productSpecificDirectory = $"{_baseProductPhotosStorageAddress}_{productId}";
+        var createdPhotosAddressList = new List<string>();
+        foreach (var photo in photos)
+        {
+            var createdPhotoAddress = await _genericBlobStorageManager
+                .SaveFile(productSpecificDirectory, BuildProductPhotoName(productId, photo.Item2), photo.Item1);
+            
+            if(createdPhotoAddress != null) createdPhotosAddressList.Add(createdPhotoAddress);
+        }
+
+        return createdPhotosAddressList;
+    }
+
+    private string BuildProductPhotoName(int productId, string extension)
+        => $"{productId}_{Guid.NewGuid()}{extension}";
+}
