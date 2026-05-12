bcef70aae79a2062f8f94e4cb516960f5d149813
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 23:15:43 +0400

    Created AddNewProductCompleteRequest model

---
+++ Application/Models/RequestModels/AddNewProductCompleteRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Models.RequestModels;
+
+public record AddNewProductCompleteRequest(
+    string ProductName,
+    string ProductDescription,
+    decimal Price,
+    int InStockCount,
+    int BrandId,
+    int CategoryId,
+    List<(byte[], string)> Photos
+);
