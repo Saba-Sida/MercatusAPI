c19ee175e9e4106abe5005ab2dcdc0b7fb7fdc60
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-13 00:46:40 +0400

    Added product photo reposiotry for saving the photo addresses

---
+++ Application/Repositories/ProductPhotos/IProductPhotoRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Repositories.ProductPhotos;
+
+public interface IProductPhotoRepository
+{
+    Task AddProductPhotosByProductId(int productId, List<string> photoAddresses);
+}
---
+++ Infrastructure/Repositories/ProductPhotos/ProductPhotoRepository.cs
@@ -0,0 +1,27 @@
+﻿using Application.Repositories.ProductPhotos;
+using Domain.Entities;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories.ProductPhotos;
+
+public class ProductPhotoRepository : IProductPhotoRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public ProductPhotoRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task AddProductPhotosByProductId(int productId, List<string> photoAddresses)
+    {
+        var photoEntities = photoAddresses.Select(photo => new ProductPhoto
+        {
+            PhotoUri = photo,
+            ProductId = productId
+        });
+
+        await _dbContext.ProductPhotos.AddRangeAsync(photoEntities);
+        await _dbContext.SaveChangesAsync();
+    }
+}
