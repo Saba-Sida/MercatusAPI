ebce9b9d21111f73881a101f946ace99bc9126dc
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 13:02:05 +0400

    Added BrandPhoto field to Brand entity

--- Domain/Entities/Brand.cs
+++ Domain/Entities/Brand.cs
@@ -7,6 +7,7 @@ public class Brand
     [Key]
     public int BrandId { get; set; }
     public required string BrandName { get; set; }
-
+    public string? BrandPhoto { get; set; }
+    
     public List<Product> Products { get; set; } = new();
 }
