d010860153261e275a2179e601f099914425253c
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 23:37:47 +0400

    Added second method for photo converting method, for list of photos

--- MercatusAPI/LayerSpecificHelpers/PhotoConvertingHelper.cs
+++ MercatusAPI/LayerSpecificHelpers/PhotoConvertingHelper.cs
@@ -5,13 +5,31 @@ public class PhotoConvertingHelper
     public async Task<(byte[], string)?> ConvertIFormFileToByteArrayAndFileExtension(IFormFile? file)
     {
         if (file == null) return null;
-        
+
         using var ms = new MemoryStream();
         await file.CopyToAsync(ms);
-        
+
         var fileByteArray = ms.ToArray();
         var fileExtension = Path.GetExtension(file.FileName);
-        
+
         return (fileByteArray, fileExtension);
+    }
+
+    public async Task<List<(byte[], string)>> ConvertIFormFileListToByteArrayAndFileExtensionList(List<IFormFile> files)
+    {
+        var result = new List<(byte[], string)>();
+
+        foreach (var file in files)
+        {
+            using var ms = new MemoryStream();
+            await file.CopyToAsync(ms);
+
+            var fileBytes = ms.ToArray();
+            var fileExtension = Path.GetExtension(file.FileName);
+
+            result.Add((fileBytes, fileExtension));
+        }
+
+        return result;
     }
 }
