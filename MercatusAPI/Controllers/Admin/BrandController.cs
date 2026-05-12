659c4e532a6f0226a3c0db973e64d6b35030e82a
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 22:25:20 +0400

    Implemented the brand controller with the brand-creation endpoint after all the implementations of the other provider layers

---
+++ MercatusAPI/Controllers/Admin/BrandController.cs
@@ -0,0 +1,43 @@
+﻿using Application.Models.RequestModels;
+using Application.Services.Brands;
+using MercatusAPI.ExternalMethods;
+using MercatusAPI.LayerSpecificHelpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercatusAPI.Controllers.Admin;
+
+[Authorize(Policy = "AdminPolicy")]
+[ApiController]
+[Route("api/admin/[controller]")]
+public class BrandController : ControllerBase
+{
+    private readonly PhotoConvertingHelper _photoConvertingHelper;
+    private readonly IBrandService _brandService;
+
+    public BrandController(
+        PhotoConvertingHelper photoConvertingHelper,
+        IBrandService brandService
+    )
+    {
+        _photoConvertingHelper = photoConvertingHelper;
+        _brandService = brandService;
+    }
+
+    [HttpPost]
+    [Route("add")]
+    [Consumes("multipart/form-data")]
+    public async Task<IActionResult> AddNewCategory([FromForm] string newBrandName, IFormFile? newBrandPhoto)
+    {
+        var request = new AddNewBrandRequest
+        {
+            Name = newBrandName,
+            BrandPhoto = await _photoConvertingHelper.ConvertIFormFileToByteArrayAndFileExtension(newBrandPhoto)
+        };
+
+        var newCategoryCreationResponse = await _brandService.CreateNewBrand(request);
+
+        return this.FormatResponse(newCategoryCreationResponse);
+    }
+}
+
