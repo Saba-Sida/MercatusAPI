f454f63e7b7ec8b71c7a5acda119a4f0ad5bf85b
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 22:17:44 +0400

    Added brand storage helper class

---
+++ Application/BlobStorage/IBrandPhotoStorageHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.BlobStorage;
+
+public interface IBrandPhotoStorageHelper
+{
+    Task<string?> SaveBrandPhoto(byte[] bytes, string extension);
+}
---
+++ Infrastructure/BlobStirage/BrandPhotoStorageHelper.cs
@@ -0,0 +1,43 @@
+﻿using Application.BlobStorage;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.BlobStirage;
+
+public class BrandPhotoStorageHelper : IBrandPhotoStorageHelper
+{
+    private readonly IGenericBlobStorageManager _genericBlobStorageManager;
+    private readonly string _baseBrandPhotosStorageAddress;
+    
+    public BrandPhotoStorageHelper(
+        IGenericBlobStorageManager genericBlobStorageManager,
+        IConfiguration configuration
+    )
+    {
+        _genericBlobStorageManager = genericBlobStorageManager;
+        var baseUri = configuration["BlobStorage:BaseUri"]!;
+        var brandPhotoSubDirectory = configuration["BlobStorage:BrandPhotoSubDirectory"]!;
+
+        _baseBrandPhotosStorageAddress =
+            Path.Combine(baseUri, brandPhotoSubDirectory); // not ends with slash
+    }
+
+    public async Task<string?> SaveBrandPhoto(byte[] bytes, string extension)
+    {
+        try
+        {
+            var fileName = BuildBrandPhotoName(extension);
+
+            return await _genericBlobStorageManager.SaveFile(
+                _baseBrandPhotosStorageAddress,
+                fileName,
+                bytes
+            );
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    string BuildBrandPhotoName(string extension) => $"BrandPhoto_{Guid.NewGuid()}{extension}";
+}
