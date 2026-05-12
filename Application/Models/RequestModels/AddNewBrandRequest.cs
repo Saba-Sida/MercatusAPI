0350b5aaecd01a60313a2dbd3ebdde303f30b578
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 22:13:04 +0400

    Created brand creational request model

---
+++ Application/Models/RequestModels/AddNewBrandRequest.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Application.Models.RequestModels;
+
+public class AddNewBrandRequest
+{
+    public required string Name { get; set; }
+    public (byte[], string)? BrandPhoto { get; set; }
+}
