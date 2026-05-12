d930f58d44a6dcbf825539ef47432985b4fb78b6
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-12 22:20:23 +0400

    Added brand entity database operations repository

---
+++ Application/Repositories/Brands/IBrandRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Repositories.Brands;
+
+public interface IBrandRepository
+{
+    Task<bool> BrandNameExists(string name);
+    Task<int> AddNewBrandName(string name);
+    Task UpdateBrandPhotoAddress(int brandId, string brandPhotoAddress);
+}
---
+++ Infrastructure/Repositories/Brands/BrandRepository.cs
@@ -0,0 +1,44 @@
+﻿using Application.Repositories.Brands;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Brands;
+
+public class BrandRepository: IBrandRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public BrandRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<bool> BrandNameExists(string name)
+    {
+        return await _dbContext.Brands
+            .AnyAsync(brand => brand.BrandName == name);
+    }
+
+    public async Task<int> AddNewBrandName(string name)
+    {
+        var newBrand = new Brand
+        {
+            BrandName = name
+        };
+        
+        _dbContext.Brands.Add(newBrand);
+        await _dbContext.SaveChangesAsync();
+        
+        return newBrand.BrandId;
+    }
+
+    public async Task UpdateBrandPhotoAddress(int brandId, string brandPhotoAddress)
+    {
+        await _dbContext.Brands
+        .Where(b => b.BrandId == brandId)
+        .ExecuteUpdateAsync(setters => setters
+            .SetProperty(b => b.BrandPhoto, brandPhotoAddress)
+        );
+    }
+}
