7f182dff1eaab74238ad1e1197f0b956ee46261e
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-13 19:43:29 +0400

    Added product viewing model

---
+++ Application/Models/DTOs/ProductViewingModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Models.DTOs;
+
+public record ProductViewingModel(
+    int ProductId,
+    string ProductName,
+    string ProductDescription,
+    decimal Price,
+    int InStockCount,
+    DateTime DateCreated,
+    DateTime DateModified,
+    int CategoryId,
+    string CategoryName,
+    int BrandId,
+    string BrandName
+    );
