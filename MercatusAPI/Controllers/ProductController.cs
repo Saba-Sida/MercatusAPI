962d448236bb07cad37c7f999e46d6fcb014d189
Saba Sidamonidze <sabasidamonidze2004@gmail.com>
2026-05-13 20:10:55 +0400

    Added new file, ProductController, implemented paginated retrieval GET endpoint

---
+++ MercatusAPI/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Models.RequestModels;
+using Application.Services.Products;
+using MercatusAPI.ExternalMethods;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercatusAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    
+    [HttpGet]
+    [Route("products")]
+    public async Task<IActionResult> Products([FromQuery] GetProductPaginationRequest request)
+    {
+        var response = await _productService.GetPaginatedProducts(request);
+        
+        return this.FormatResponse(response);
+    }
+}
